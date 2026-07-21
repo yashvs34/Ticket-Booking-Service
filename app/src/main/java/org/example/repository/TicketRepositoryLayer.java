73fb62ee426c6240b1568ab2164d5e6a8151a57c
Yashvardhan Singh <yashvardhansingh232@gmail.com>
2026-07-21 17:43:01 +0000

    Completed TicketRepository and TrainRepository

--- app/src/main/java/org/example/repository/TicketRepositoryLayer.java
+++ app/src/main/java/org/example/repository/TicketRepositoryLayer.java
@@ -3,18 +3,47 @@ package org.example.repository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.Ticket;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.example.constants.RepositoryConstants.TICKETS_PATH;
+
 public class TicketRepositoryLayer {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static List<Ticket> ticketsList;
+    private static final File tickets = new File(TICKETS_PATH);
 
-    public void saveTicketToDB(Ticket ticket) {
+    private static boolean saveTicket(Ticket ticket) throws IOException {
+        if (ticketsList.contains(ticket)){
+            System.out.println("Ticket already present in DB");
+            return false;
+        }
 
+        ticketsList.add(ticket);
+        objectMapper.writeValue(tickets, ticketsList);
+        return true;
     }
 
-//    public List<User> getAllTicketsFromDB() {
-//
-//    }
-//
-//    public User getTicketFromDB(String ticketId) {
-//
-//    }
+    public static boolean saveTicketToDB(Ticket ticket) {
+        try {
+            return saveTicket(ticket);
+        } catch (IOException e) {
+            System.out.println("IO Exception while saving ticket.");
+        }
+        return false;
+    }
+
+    public static List<Ticket> getAllTicketsFromDB() {
+        return ticketsList;
+    }
+
+    public static Optional<Ticket> getTicketFromDB(String ticketId) {
+        Optional<Ticket> ticket = ticketsList.stream().filter(u -> u.getTicketId().equals(ticketId)).findFirst();
+        if (ticket.isEmpty()) {
+            System.out.println("No ticket present with this ticketId");
+        }
+        return ticket;
+    }
 }
